714a877ff4bf0f8aca86d79307f464993b3ed751
Serge Rey <sjsrey@gmail.com>
2022-11-20 15:08:27 -0800

    openurl.sh

---
+++ openurl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#
+# Select from a list of my bookmarks browse to it
+#
+
+
+BROWSER=firefox
+
+url="$(cat ~/.local/share/reyos/bookmarks | sort -n | rofi -width 20 -dmenu -mesg ">>> Select url to browse to" -i -p  "openurl.sh ")"
+
+notify-send "$url selected"
+$BROWSER $url
+
