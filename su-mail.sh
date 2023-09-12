f9a405dd4599ec609f17676025b9ee0afeb08b35
Serge Rey <sjsrey@gmail.com>
2023-09-12 12:05:48 -0700

    side by side setup

--- su-mail.sh
+++ su-mail.sh
@@ -15,10 +15,22 @@ xdotool key "ctrl+t"; xdotool type "https://mail.google.com/mail/u/1/#inbox"; xd
 sleep 4
 
 
+xdotool key Super_L+Left
+sleep 1
+
+
+google-chrome </dev/null &>/dev/null &
+sleep 4
+xdotool key "ctrl+t"; xdotool type "https://app.slack.com/client/T03M7EWMC7L/D03M0SR8VD4?selected_team_id=T03M7EWMC7L"; xdotool key Return
+sleep 4
+
+
 xdotool key "ctrl+t"; xdotool type "https://calendar.google.com/calendar/u/0/r"; xdotool key Return
+sleep 4
+
+xdotool key Super_L+Right
+
+
 
 echo "Your email workspace is ready 😎"
-
-
-
 
